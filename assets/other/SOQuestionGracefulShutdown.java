6a27ed1d415aba486d7507ae45c1a8ed61b03c9c
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2023-08-09 16:37:31 +0100

    Update SOQuestionGracefulShutdown.java

--- assets/other/SOQuestionGracefulShutdown.java
+++ assets/other/SOQuestionGracefulShutdown.java
@@ -1,6 +1,39 @@
 public class SOQuestionGracefulShutdown {
 
     public static void main(String[] args) throws InterruptedException {
+        ScheduledThreadPoolExecutor taskExecutor = new ScheduledThreadPoolExecutor(2);
+
+        AtomicLong counter = new AtomicLong(0);
+        taskExecutor.scheduleWithFixedDelay(() -> {
+            try {
+                counter.incrementAndGet();
+                System.out.println("Task " + counter.get() + " started");
+                Thread.sleep(10_000);
+                System.out.println("Task " + counter.get() + " finished");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("Shutting down");
+                taskExecutor.shutdown();
+                taskExecutor.awaitTermination(30, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+        System.out.println("Started send a SIGTERM right after this message using: kill -15 " + ProcessHandle.current().pid());
+
+        for (; ; ) {
+            Thread.sleep(100);
+        }
+    }
+
+    public static void mainWithTaskExecutor(String[] args) throws InterruptedException {
 
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(2);
